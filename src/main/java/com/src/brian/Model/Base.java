9de5fe155f917594584566457d78f41c3d252b1e
mtotodev05 <mtotodev05@gmail.com>
2017-10-04 08:05:55 +0300

    Generated Base

--- src/main/java/com/src/brian/Model/Base.java
+++ src/main/java/com/src/brian/Model/Base.java
@@ -2,5 +2,27 @@ package com.src.brian.Model;
 import java.sql.*;
 
 
-public class Base {
+public abstract class Base {
+    public static String dbUrl ="jdbc:mysql://localhost:3306/Fantastic?useSSL=false?";
+    public static String dbUsername ="root";
+    public static String dbPassword="wamatu";
+    public static String dbDriver="com.mysql.cj.Driver";
+    protected  Connection conn = null;
+
+    protected final boolean dbConnect(){
+        try{
+
+            Class.forName(dbDriver);
+            conn=DriverManager.getConnection(dbUrl,dbUsername,dbPassword);
+
+        }catch(ClassNotFoundException | SQLException ex){
+            System.err.println("Connection Error"+ex.getMessage());
+            return  false;
+        }
+
+        return true;
+    }
+
+
+
 }
